9f59bfaea41b983cd69cba670b9ed43ddbc0808a
fauzan.binus <fauzan.binus@gmail.com>
2020-04-04 15:14:02 +0700

    3

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "April 4, 2020 15:10:00",
+	date: "April 4, 2020 15:20:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
